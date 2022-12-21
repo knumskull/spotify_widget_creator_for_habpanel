859e1dd45fa9397833173bfa60ae4c4101774b52
Steffen Froemer <steffen@froemer.net>
2022-12-21 08:48:11 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spotify API Query Tool for HabPanel Widget Playlist-creator
+This script creates a [OpenHAB Habpanel](https://www.openhab.org/docs/configuration/habpanel.html) widget, containing clickable Covers from a Spotify Playlist
+
+## LICENSE
+This project is licensed under the GPLv3+
--- query_api.py
+++ query_api.py
@@ -11,18 +11,8 @@ from PIL import Image
 from requests.adapters import HTTPAdapter
 from spotipy.oauth2 import SpotifyClientCredentials
 from urllib3.util.retry import Retry
+from yaml import FullLoader, load as yaml_load
 
-# Die Alte und der Kommisar
-# https://open.spotify.com/album/3snwe7XTiMOlxdRb97GZoq?si=U8UMUjW3Qu2LN_Z7_fPtdw
-
-# Bibi und Tina
-# https://open.spotify.com/playlist/25dIxWREnlY6a37FMj1sST?si=63bae965e44a48ff
-
-# Lieselotte Postkuh - Alle Hoerspiele
-# https://open.spotify.com/playlist/5SCbLiQEB8u5iWOHM9cNrn?si=2c19096064ad4f6c
-
-# Die drei ??? Kids
-# https://open.spotify.com/playlist/6GW1MH9yw8zA2AHDPW73cO?si=eda1239c13d84594
 
 # not the best way - ToDo improve the error handling
 try:
@@ -33,38 +23,39 @@ except KeyError:
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-# get a list of albums
-# extract title, id, cover from every item
-# apply template
 
+def load_query_information() -> dict:
+    """ load information to query from YAML file and provide as list """
+    try:
+        with open('query_data.yaml') as f:
+            data = yaml_load(f, Loader=FullLoader)
+    except FileNotFoundError as err:
+        print("File 'query_data.yml' not found.")
+    return data
 
-def get_type_and_if_from_url(url):
-    """ extract the id for playlist or album from url
-    """
+def query_spotify_api(url) -> dict:
+    """ Based on given url, query relevant information from Spotify API """
+
     pattern = r'.*spotify.com\/([a-z]+)\/([a-zA-Z0-9]+)\?.*'
-    _name, _id = re.match(pattern, url).groups()
+    _type, _id = re.match(pattern, url).groups()
 
-    return {'type_name': _name, 'id': _id}
+    _content = {}
+    if _type == 'playlist':
+        query = spotify.playlist(playlist_id=_id)
+        result = query['tracks']
+        playlist_name = query['name']
+        all_tracks = result['items']
+        while result['next']:
+            result = spotify.next(result)
+            all_tracks.extend(result['items'])
 
-#_type, _id = get_type_and_if_from_url('https://open.spotify.com/playlist/5SCbLiQEB8u5iWOHM9cNrn?si=2c19096064ad4f6c').values()
-_type, _id = get_type_and_if_from_url('https://open.spotify.com/playlist/25dIxWREnlY6a37FMj1sST?si=63bae965e44a48ff').values()
+        for item in all_tracks:
+            _content[item['track']['album']['id']] = item['track']['album']['name']
 
+    return _content
 
-_content = {}
-if _type == 'playlist':
-    query = spotify.playlist(playlist_id=_id)
-    result = query['tracks']
-    playlist_name = query['name']
-    all_tracks = result['items']
-    while result['next']:
-        result = spotify.next(result)
-        all_tracks.extend(result['items'])
-
-    for item in all_tracks:
-        _content[item['track']['album']['id']] = item['track']['album']['name']
-    
-
-def get_openhab_relevant_data_from_album(album_ids):
+def get_openhab_relevant_data_from_album(album_ids) -> list:
+    """ """
     x = []
     if isinstance(album_ids, list):
         x.extend(album_ids)
@@ -87,17 +78,17 @@ def get_openhab_relevant_data_from_album(album_ids):
             query["album_cover_large"] = item['images'][0]['url']
             query["album_id"] = item['id']
 
-            img_url = item['images'][1]['url']
+            # use large image and resize
+            img_url = item['images'][0]['url']
 
             with Image.open(session.get(img_url, stream = True).raw) as img:
                 img.filename = img_url.rsplit('/', 1)[-1] + '.jpg'
-                
                 local_path = '/etc/openhab/html/spotify_api/images'
 
                 if not os.path.exists(local_path):
                     os.mkdir(local_path)
                 
-                img.resize((150,150),Image.ANTIALIAS).save(os.path.join(local_path, img.filename))
+                img.resize((200,200),Image.ANTIALIAS).save(os.path.join(local_path, img.filename))
                 query["album_cover_local"] = f'http://openhabian.home.wlan:8080/static/spotify_api/images/{img.filename}'
 
             _playlist.append(query)
@@ -105,27 +96,11 @@ def get_openhab_relevant_data_from_album(album_ids):
             continue
     return _playlist
 
-
-playlist = []
-album_ids = list(_content.keys())
-
-for album_id in album_ids:
-    playlist.extend(get_openhab_relevant_data_from_album(album_id))
-
-templateLoader = FileSystemLoader(searchpath="./templates")
-templateEnv = Environment(loader=templateLoader, autoescape=select_autoescape(['html', 'xml']))
-TEMPLATE_FILE = "playlist.jinja2"
-template = templateEnv.get_template(TEMPLATE_FILE)
-outputText = template.render(playlist=playlist)
-
-
 def create_widget_file(content:str, playlist_name:str, destination:str = '.') -> None:
     """ create appropriate widget file from given data to import in openhab
     """
     if not os.path.exists(destination):
-        os.chdir(os.path.join(os.path.curdir,destination))
-    else:
-        os.chdir(destination)
+        os.mkdir(os.path.join(os.path.curdir,destination))
 
     openhab_widget = {}
     openhab_widget['template'] = content
@@ -133,10 +108,35 @@ def create_widget_file(content:str, playlist_name:str, destination:str = '.') ->
     openhab_widget['settings'] = ''
 
     target_filename = playlist_name.replace(" ", "_").lower()
+    target = os.path.join(destination, target_filename)
 
-    with open(f'{target_filename}.widget.json', 'w') as widget:
+    with open(f'{target}.widget.json', 'w') as widget:
         widget.write(json.dumps(openhab_widget))
 
 
+def get_playlist_from_spotify_playlist_url(playlist_url) -> list:
+    """ """
+    _playlist = []
+    _content = query_spotify_api(playlist_url)
+    album_ids = list(_content.keys())
 
-create_widget_file(outputText, playlist_name, 'output')
+    for album_id in album_ids:
+        _playlist.extend(get_openhab_relevant_data_from_album(album_id))
+
+    return _playlist
+
+if __name__ == '__main__':
+    """ run this script as standalone and create widget.json file """
+
+    for item in load_query_information():
+        print(f"{item['display_name']} - {item['playlist_url']}")
+
+        playlist = get_playlist_from_spotify_playlist_url(item['playlist_url'])
+
+        templateLoader = FileSystemLoader(searchpath="./templates")
+        templateEnv = Environment(loader=templateLoader, autoescape=select_autoescape(['html', 'xml']))
+        TEMPLATE_FILE = "playlist.jinja2"
+        template = templateEnv.get_template(TEMPLATE_FILE)
+        outputText = template.render(playlist=playlist)
+
+        create_widget_file(outputText, item['display_name'], 'output')
